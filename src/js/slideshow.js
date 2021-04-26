4d0c35be815352e69007f5f323426d43faf89769
Guillaume Hammadi <ghammadi@gmail.com>
2021-04-25 20:20:28 -0700

    Basic automation for slideshow and proper display on all devices

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -4,17 +4,31 @@ export function add (n1, n2) {
   return n1 + n2
 }
 
-const images = imagesData.images
-
 export const initializeSlideshow = () => {
+  const imagePaths = []
+
   for (const [key, value] of Object.entries(imagesData.images)) {
-    console.log('key', key)
-    console.log('value', value)
+    console.log(key)
+    imagePaths.push(value)
   }
 
-  const slideshowContainer = document.querySelector('#slideshow')
-  const image = document.createElement('img')
-  image.src = images[Object.keys(images)[0]]
+  const slideshowContainer = document.querySelector('.image-container')
+
   slideshowContainer.querySelector('p').remove()
+
+  const image = document.createElement('img')
+  let currentImgIdx = 0
+
+  image.src = imagePaths[currentImgIdx]
   slideshowContainer.appendChild(image)
+
+  const slideToNextImg = () => {
+    console.log('current Img Idx', currentImgIdx)
+    console.log('current Img Idx', imagePaths)
+    currentImgIdx = currentImgIdx === 3 ? 0 : currentImgIdx + 1
+    image.src = imagePaths[currentImgIdx]
+    slideshowContainer.appendChild(image)
+  }
+
+  setInterval(slideToNextImg, 2000)
 }
